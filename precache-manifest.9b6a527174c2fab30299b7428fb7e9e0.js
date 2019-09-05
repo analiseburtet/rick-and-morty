56380340e4e16fef9d6301c5f96db9a8274d8036
analiseburtet <analiseburtet@gmail.com>
2019-09-05 18:03:37 -0300

    Updates

---
+++ precache-manifest.9b6a527174c2fab30299b7428fb7e9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d90360c399ccd54a78bc7f18bdef23e",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "82731291b488122dbd44",
+    "url": "/rick-and-morty/static/css/main.2584c2ba.chunk.css"
+  },
+  {
+    "revision": "bbc6efeb01c56270ab46",
+    "url": "/rick-and-morty/static/js/2.055bf7e3.chunk.js"
+  },
+  {
+    "revision": "82731291b488122dbd44",
+    "url": "/rick-and-morty/static/js/main.542f25bf.chunk.js"
+  },
+  {
+    "revision": "eef8940eba2676bc34d0",
+    "url": "/rick-and-morty/static/js/runtime~main.48e4ecd5.js"
+  }
+]);
